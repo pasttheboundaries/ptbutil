15bcaa0e74f3c5fd4b16c57550b7a0a503db2d33
past <pasttheboundaries@gmail.com>
2022-12-20 18:10:32 +0100

    iteration.listwise.ifish added

--- iteration/__init__.py
+++ iteration/__init__.py
@@ -1,3 +1,3 @@
-from .listwise import (chaintype, each_with_each, flatlist, GridFeed, indmap, MaskableList,
+from .listwise import (chaintype, each_with_each, flatlist, GridFeed, ifish, indmap, MaskableList,
                        UnevenNestingError, Nesting, nesting, ngrams, nlen, sequence_binary_mask, Stack)
 from .dictwise import sort_by_value, merge_2_dicts, dicta
